bd7bcd60e03c7dac10326f3f23ffbab943452b76
Myrna <info@spiderlilyweb.com>
2011-03-14 09:35:23 -0700

    first commit

---
+++ application/controllers/home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home extends Controller {
+
+	function Home()
+	{
+		parent::Controller();
+                	}
+
+	function index()
+	{
+		$this->load->view('home');
+	}
+}
---
+++ application/views/includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+        <link rel="stylesheet" href="css/gppstyles.css">
+	<title></title>
+</head>
+<body>
+    
